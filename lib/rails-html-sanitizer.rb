edb3b0d4c8ea8f2820e61f371f091214714abc90
Timm <kaspth@gmail.com>
2014-05-24 00:01:31 +0200

    Add Sanitizer module responding to sanitizer_vendor interface.

--- lib/rails-html-sanitizer.rb
+++ lib/rails-html-sanitizer.rb
@@ -5,5 +5,18 @@ require "rails/html/sanitizer"
 
 module Rails
   module Html
+    module Sanitizer
+      def full_sanitizer
+        Html::FullSanitizer
+      end
+
+      def link_sanitizer
+        Html::LinkSanitizer
+      end
+
+      def white_list_sanitizer
+        Html::WhiteListSanitizer
+      end
+    end
   end
 end
