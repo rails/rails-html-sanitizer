48c0f014c99c90124bd568940060d3fcebb788a6
Timm <kaspth@gmail.com>
2014-05-27 12:56:20 +0200

    Change Sanitizer to class with class methods.

--- lib/rails-html-sanitizer.rb
+++ lib/rails-html-sanitizer.rb
@@ -5,17 +5,19 @@ require "rails/html/sanitizer"
 
 module Rails
   module Html
-    module Sanitizer
-      def full_sanitizer
-        Html::FullSanitizer
-      end
+    class Sanitizer
+      class << self
+        def full_sanitizer
+          Html::FullSanitizer
+        end
 
-      def link_sanitizer
-        Html::LinkSanitizer
-      end
+        def link_sanitizer
+          Html::LinkSanitizer
+        end
 
-      def white_list_sanitizer
-        Html::WhiteListSanitizer
+        def white_list_sanitizer
+          Html::WhiteListSanitizer
+        end
       end
     end
   end
