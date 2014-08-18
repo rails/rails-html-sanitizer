b22d4d8cc475fe26e88996b04561437f911107d0
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-08-18 20:45:00 -0300

    Remove the method before redefining

--- lib/rails-html-sanitizer.rb
+++ lib/rails-html-sanitizer.rb
@@ -26,6 +26,10 @@ end
 module ActionView
   module Helpers
     module SanitizeHelper
+      if method_defined?(:sanitizer_vendor) || private_method_defined?(:sanitizer_vendor)
+        undef_method(:sanitizer_vendor)
+      end
+
       def sanitizer_vendor
         Rails::Html::Sanitizer
       end
