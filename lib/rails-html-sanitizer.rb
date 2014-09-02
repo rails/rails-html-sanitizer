2dbd320bfd07b52b115ccb8d734ef950d926b800
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-09-01 23:23:32 -0300

    Move default implementation to this gem

--- lib/rails-html-sanitizer.rb
+++ lib/rails-html-sanitizer.rb
@@ -34,6 +34,34 @@ module ActionView
         def sanitizer_vendor
           Rails::Html::Sanitizer
         end
+
+        if method_defined?(:sanitized_allowed_tags=) || private_method_defined?(:sanitized_allowed_tags=)
+          undef_method(:sanitized_allowed_tags=)
+        end
+
+        # Replaces the allowed tags for the +sanitize+ helper.
+        #
+        #   class Application < Rails::Application
+        #     config.action_view.sanitized_allowed_tags = 'table', 'tr', 'td'
+        #   end
+        #
+        def sanitized_allowed_tags=(tags)
+          sanitizer_vendor.white_list_sanitizer.allowed_tags = tags
+        end
+
+        if method_defined?(:sanitized_allowed_attributes=) || private_method_defined?(:sanitized_allowed_attributes=)
+          undef_method(:sanitized_allowed_attributes=)
+        end
+
+        # Replaces the allowed HTML attributes for the +sanitize+ helper.
+        #
+        #   class Application < Rails::Application
+        #     config.action_view.sanitized_allowed_attributes = ['onclick', 'longdesc']
+        #   end
+        #
+        def sanitized_allowed_attributes=(attributes)
+          sanitizer_vendor.white_list_sanitizer.allowed_attributes = attributes
+        end
       end
     end
   end
