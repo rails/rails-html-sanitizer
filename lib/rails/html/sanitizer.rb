7114f1e7f592cf107e0f738bae845bc1d7f81a88
Grzegorz Derebecki <grzegorz.derebecki@fdb.pl>
2015-02-18 21:15:14 +0100

    stop encode special chars for full sanitizer

--- lib/rails/html/sanitizer.rb
+++ lib/rails/html/sanitizer.rb
@@ -28,7 +28,7 @@ module Rails
 
         Loofah.fragment(html).tap do |fragment|
           remove_xpaths(fragment, XPATHS_TO_REMOVE)
-        end.text
+        end.text(encode_special_chars: false)
       end
     end
 
