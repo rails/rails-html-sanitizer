a3bd2640bdabdf7d4b8549ae1539bd1508ef588a
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2018-03-20 09:13:53 -0400

    bumps version of loofah
    This is in effort to address https://github.com/flavorjones/loofah/issues/144.

--- test/sanitizer_test.rb
+++ test/sanitizer_test.rb
@@ -385,13 +385,13 @@ class SanitizersTest < Minitest::Test
 
   def test_should_sanitize_illegal_style_properties
     raw      = %(display:block; position:absolute; left:0; top:0; width:100%; height:100%; z-index:1; background-color:black; background-image:url(http://www.ragingplatypus.com/i/cam-full.jpg); background-x:center; background-y:center; background-repeat:repeat;)
-    expected = %(display: block; width: 100%; height: 100%; background-color: black; background-x: center; background-y: center;)
+    expected = %(display:block;width:100%;height:100%;background-color:black;background-x:center;background-y:center;)
     assert_equal expected, sanitize_css(raw)
   end
 
   def test_should_sanitize_with_trailing_space
     raw = "display:block; "
-    expected = "display: block;"
+    expected = "display:block;"
     assert_equal expected, sanitize_css(raw)
   end
 
