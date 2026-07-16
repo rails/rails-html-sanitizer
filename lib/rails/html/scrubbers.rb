74dcb8053e6da9921246ce71b06ad9fd65b19586
Mike Dalessio <mike.dalessio@gmail.com>
2026-07-15 22:45:33 -0400

    Properly restrict SVG href attributes
    PermitScrubber reimplements Loofah's SVG local-reference restriction,
    which covered only `xlink:href`. SVG 2 also allows plain `href`, so
    `<use href="https://example.com/x.svg#p"/>` passed through intact.
    Restrict both via Loofah's SVG_HREF_ATTRIBUTES set (Loofah >= 2.25.2),
    mirroring the related Loofah fix for GHSA-9wjq-cp2p-hrgf.
    
    ref: https://github.com/rails/rails-html-sanitizer/security/advisories/GHSA-cj75-f6xr-r4g7

--- lib/rails/html/scrubbers.rb
+++ lib/rails/html/scrubbers.rb
@@ -172,7 +172,7 @@ module Rails
             Loofah::HTML5::Scrub.scrub_attribute_that_allows_local_ref(attr_node)
           end
 
-          if Loofah::HTML5::SafeList::SVG_ALLOW_LOCAL_HREF.include?(node.name) && attr_name == "xlink:href" && attr_node.value =~ /^\s*[^#\s].*/m
+          if Loofah::HTML5::SafeList::SVG_ALLOW_LOCAL_HREF.include?(node.name) && Loofah::HTML5::SafeList::SVG_HREF_ATTRIBUTES.include?(attr_name) && attr_node.value =~ /^\s*[^#\s].*/m
             attr_node.remove
           end
 
